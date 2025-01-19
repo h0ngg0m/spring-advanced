86887af0ddafde83d9f213417ecb27cad5d06095
h0ngg0m <jaehongbyeon98@gmail.com>
2025-01-19 15:22:40 +0900

    :sparkles: thread local trace

--- src/main/java/com/honggom/springadvanced/LogTraceConfig.java
+++ src/main/java/com/honggom/springadvanced/LogTraceConfig.java
@@ -1,7 +1,7 @@
 package com.honggom.springadvanced;
 
-import com.honggom.springadvanced.trace.logtrace.FieldLogTrace;
 import com.honggom.springadvanced.trace.logtrace.LogTrace;
+import com.honggom.springadvanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +10,6 @@ public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
